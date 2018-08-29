3ed35f03f16c26f3b528112ca0c341514cb0aea8
Morgan Purcell <morganpurcell3@gmail.com>
2018-08-29 15:29:34 -0700

    addUser route and addGroup route in server index.js

--- server/index.js
+++ server/index.js
@@ -14,6 +14,8 @@ const LocalStrategy = require('passport-local').Strategy;
 
 const models = require('./models/models');
 const User = models.User;
+const Doctor = models.Doctor;
+const Group = models.Group;
 
 
 app.use(express.static(path.join(__dirname, 'build')));
@@ -59,10 +61,7 @@ app.use(passport.session());
 // app.use('/', routes);
 
 app.post('/api/addUser', function(req,res) {
-
-  console.log("in app.post username:" + req.body.username);
-
-  const newUser = new User({
+const newUser = new User({
     username: req.body.username,
     password: req.body.password,
     userType: req.body.userType,
@@ -78,12 +77,51 @@ app.post('/api/addUser', function(req,res) {
     })
 });
 
+app.post('/api/addDoctor', function(req,res) {
+  const newDoctor = new Doctor({
+    //don't forgeet to populate user id correctly
+    user: req.body.user,
+    name: req.body.name,
+    specialty: req.body.specialty,
+    language: req.body.language,
+    location: req.body.location,
+    email: req.body.email
+  })
+
+  newDoctor.save()
+    .then(response => {
+      console.log("added new doctor successfuly")
+      res.send(response)
+    }).catch(err => {
+      console.log("error didnt save")
+      res.send(err)
+    })
+});
+
+app.post('/api/addGroup', function(req,res) {
+  const newGroup = new Group({
+    //don't forgeet to populate user id correctly
+    user: req.body.user,
+    name: req.body.name,
+    about: req.body.about,
+    email: req.body.email,
+    isGroup: req.body.isGroup
+  })
+
+  newGroup.save()
+    .then(response => {
+      console.log("added new group successfuly")
+      res.send(response)
+    }).catch(err => {
+      console.log("error didnt save")
+      res.send(err)
+    })
+});
+
 app.post('/api/login', passport.authenticate('local', {
   successRedirect:'/api/current_user',
   failureRedirect:'/api/failed'
 }));
-
-
 
 app.get('/api/logout', function(req, res) {
     req.logout();
@@ -94,14 +132,11 @@ app.get('/api/current_user', (req, res) => {
   if(!req.user) {
     return res.send('Error you need to login');
   }
-
   res.send(req.user);
 })
 
 app.get('/api/failed', (req, res) => {
-
   res.status(401).send('Error bad pass');
-
 })
 
 
